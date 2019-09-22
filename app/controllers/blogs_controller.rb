947fbecf90a26fdfd7dec49410c92bf2b7ad9ecd
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-09-22 19:51:11 +0900

    create new function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,10 +5,20 @@ class BlogsController < ApplicationController
 
 
   def new
-
+      @blog = Blog.new
   end
 
   def create
+    @blogs = Blog.create(blog_params)
 
+    redirect_to new_blog_path
+
+  end
+
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:content)
   end
 end
