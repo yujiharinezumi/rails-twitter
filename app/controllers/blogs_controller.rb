d122458a0afc9c5dfc76003824a528cbc92f887d
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-09-22 23:02:12 +0900

    create confirm function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,10 +14,14 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "ブログを作成しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -43,6 +47,11 @@ class BlogsController < ApplicationController
      @blog.destroy
      redirect_to blogs_path, notice:"ブログを削除しました！"
    end
+
+   def confirm
+   @blog = Blog.new(blog_params)
+   render :new if @blog.invalid?
+ end
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :blogs
+  resources :blogs do
+    collection do
+      post  :confirm
+    end
+  end
 end
